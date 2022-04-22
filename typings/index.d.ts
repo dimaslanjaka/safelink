8c22335081832a1bb5c864dbfdfef38967fd2301
dimaslanjaka <dimaslanjaka@gmail.com>
2022-04-23 00:44:17 +0700

    init gh-pages

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-import './markdown-it-sub';
-import './markdown-it-sup';
-import './markdown-it-mark';
-import './markdown-it-footnote';
-import './markdown-it-abbr';
-import './markdown-it-image-figures';
