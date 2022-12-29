f21956f8fab90511f3addf958ed1219e4f894c1c
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-29 15:55:28 +0700

    fix infinite loop

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 const gulp = require('gulp');
-const { copyDistToDemo } = require('./docs');
 const pkg = require('./package.json');
 
 /**
  *  BELOW SCRIPT TO DEPLOY OUR GITHUB PAGES
  */
-
-gulp.task('copy', copyDistToDemo);
-
-gulp.task('default', gulp.series('copy'));
