24d1142a359782f3b1bed060a2f243131c15fb56
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-29 16:06:22 +0700

    update task

--- typedoc-server.js
+++ typedoc-server.js
@@ -7,16 +7,24 @@ const bs = browserSync.create();
  * required:
  * * add docs script for `npm run docs`
  * * npm i -D git-command-helper
+ *
  * repo: https://github.com/dimaslanjaka/nodejs-package-types/blob/main/typedoc-server.js
  * raw: https://github.com/dimaslanjaka/nodejs-package-types/raw/main/typedoc-server.js
  * update: curl -L https://github.com/dimaslanjaka/nodejs-package-types/raw/main/typedoc-server.js > typedoc-server.js
  */
 
+let building = false;
 /**
+ * build demo and docs (run only 1 instance)
  * @type {browserSync.MiddlewareHandler | browserSync.PerRouteMiddleware}
  */
 const buildDocs = async (_req, _res, next) => {
+  if (building) return next();
+  building = true;
+  console.log('building docs...');
   await spawn('npm', ['run', 'docs'], { cwd: __dirname, stdio: 'inherit' });
+  console.log('docs build finished');
+  building = false;
   next();
 };
 
