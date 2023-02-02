efa454d9cfe07c799a6c18710835a6075eec075c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-02 17:33:33 +0700

    refactor: update and pack
    add non-included submodules
    update dependencies
    create new tarball packs

--- dist/bundle.js
+++ dist/bundle.js
@@ -539,7 +539,7 @@ eval("/* (ignored) */\n\n//# sourceURL=webpack://safelinkify/crypto_(ignored)?")
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("2484527a21c93d7b488e")
+/******/ 		__webpack_require__.h = () => ("78defef18427a86b89ea")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
