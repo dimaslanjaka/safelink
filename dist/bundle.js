d3560c67e75dfbe18d80d20fdbc5b3cd8e020dba
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-17 23:56:17 +0700

    chore: release 1.1.15

--- dist/bundle.js
+++ dist/bundle.js
@@ -539,7 +539,7 @@ eval("/* (ignored) */\n\n//# sourceURL=webpack://safelinkify/crypto_(ignored)?")
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("88e710390f344f4e03f7")
+/******/ 		__webpack_require__.h = () => ("57813806170770a8088c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
