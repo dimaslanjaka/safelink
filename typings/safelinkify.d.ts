26c84408dc1d7fe39e912134e38ac12afb7fef46
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-07 00:36:54 +0700

    try apply global vars

--- typings/safelinkify.d.ts
+++ typings/safelinkify.d.ts
@@ -1,3 +1,16 @@
-import * as safelinkify from '../dist/';
-export const { safelink, resolveQueryUrl, parseQuery } = safelinkify.default;
+import * as safelinkify from '../dist';
+import safelink from '../src/safelink';
 export * from '../dist/globals';
+export const { safelink, resolveQueryUrl, parseQuery } = safelinkify.default;
+declare global {
+  const safelink: safelink;
+  declare let Window: {
+    prototype: Window;
+    new (): Window;
+  };
+  interface Window {
+    safelink: safelink;
+    resolveQueryUrl: typeof safelinkify.default.resolveQueryUrl;
+    parseQuery: typeof safelinkify.default.parseQuery;
+  }
+}
