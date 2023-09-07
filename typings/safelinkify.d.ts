b6b6701c72b2408578254457e3e5ca116e8c78f7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-07 07:14:51 +0700

    update custom types

--- typings/safelinkify.d.ts
+++ typings/safelinkify.d.ts
@@ -1,16 +1,9 @@
-import * as safelinkify from '../dist';
-import safelink from '../src/safelink';
-export * from '../dist/globals';
-export const { safelink, resolveQueryUrl, parseQuery } = safelinkify.default;
+import { default as safelinkify } from '../dist';
 declare global {
-  const safelink: safelink;
-  declare let Window: {
-    prototype: Window;
-    new (): Window;
-  };
   interface Window {
-    safelink: safelink;
-    resolveQueryUrl: typeof safelinkify.default.resolveQueryUrl;
-    parseQuery: typeof safelinkify.default.parseQuery;
+    safelink: (typeof safelinkify)['safelink'];
+    parseQuery: (typeof safelinkify)['parseQuery'];
+    resolveQueryUrl: (typeof safelinkify)['resolveQueryUrl'];
   }
 }
+export * from '../dist';
