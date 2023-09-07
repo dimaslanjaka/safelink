c71b2d8b607d3158fa2554c6f924435a37bb4810
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-07 11:04:29 +0700

    remove unused file

--- src-docs/summon.ts
+++
@@ -1,8 +0,0 @@
-import spawn from 'cross-spawn';
-import { join, resolve } from 'upath';
-
-const summon = spawn('ts-node', [join(__dirname, 'article-generator', 'index.ts')], {
-  cwd: resolve(__dirname, '..'),
-  shell: true,
-  stdio: 'inherit'
-});
