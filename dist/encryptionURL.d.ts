8db48dec7ef7aa0da6cd4311443e2ad8932d9670
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-19 13:26:00 +0700

    refactor: ignore dist files

--- dist/encryptionURL.d.ts
+++
@@ -1,8 +0,0 @@
-import { encryptionURLResult, Nullable } from './';
-/**
- * resolve url encryption
- * @param url
- * @param passphrase
- * @returns
- */
-export default function encryptionURL(url: Nullable<string | URL>, passphrase?: string, debug?: boolean): encryptionURLResult;
