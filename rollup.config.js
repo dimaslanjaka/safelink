474d04ff6f676f79abecb34011803ae31547c12b
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-19 19:40:42 +0700

    fix: rename output file for browser bundle in Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -93,7 +93,7 @@ export default [
   {
     input: 'src/index.ts',
     output: {
-      file: 'dist/bundle.js',
+      file: 'dist/browser-bundle.js',
       format: 'iife',
       sourcemap: true
     },
